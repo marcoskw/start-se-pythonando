60336618299e8b68d99bbb45e2ab326df915ba7a
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2024-08-05 14:33:59 -0300

    Criando view de Cadastro de Usuário

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro/', views.cadastro, name="cadatros"),
+    path('cadastro/', views.cadastro, name="cadastro"),
     
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')       
+
+        if not senha == confirmar_senha:
+            print("As senhas não correspondem")
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            print("A senha tem que ser maior que 6 caracteres")
+            return redirect('/usuarios/cadastro')
+        
+        users = User.objects.filter(username=username)
+        if users.exists():
+            print('Nome de usuário já cadastrado')
+            return redirect('/usuarios/cadastro')
+        
+        user = User.objects.create_user(
+            username=username,
+            password=senha,
+        )
+
+        return redirect('/usuarios/logar')
